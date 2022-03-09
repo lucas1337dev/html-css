edcb89bb4125a66bd7aa6747c9982fb597bf5d86
Lucas Gabriel <lucasgbarbosa@yahoo.com>
2022-03-09 15:47:09 -0300

    Att

---
+++ exercicios-onebitcode/javascript/change_style.js
@@ -0,0 +1,15 @@
+/* alert('Meu primeiro JavaScript')
+
+console.log('Meu primeiro JavaScript')
+console.log('Recarregou...') */
+
+/* document.getElementById('demo').style.fontSize = "35px" */
+
+/* let i = 0;
+let x = 1;
+while(i<100) {
+    console.log('Valor do x = ' + x)
+    x++;
+    i++;
+} */
+
